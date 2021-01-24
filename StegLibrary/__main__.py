008582cb15142586f6b5526947e43532e0b78fff
Nguyen Thai Binh <bincity2003@gmail.com>
2021-01-24 19:41:10 +0800

    Rename to facilitate setup operation

--- StegLibrary/__main__.py
+++ StegLibrary/__main__.py
@@ -7,11 +7,11 @@ def steg():
     pass
 
 
-@steg.command("create", help="Create stegnograph")
+@steg.command("create", help="Create steganograph")
 @click.option("-i", "--image", help="Path to custom image file", type=click.Path(True, True, False))
 @click.option("-k", "--key", help="The authentication key", type=str, default=Header.default_key)
-@click.option("-c", "--compress", help="Compression level of the stegnograph", type=int, default=9)
-@click.option("-p", "--pack", help="Density of the stegnograph", type=click.Choice(["1", "2", "3"]), default="1")
+@click.option("-c", "--compress", help="Compression level of the steganograph", type=int, default=9)
+@click.option("-p", "--pack", help="Density of the steganograph", type=click.Choice(["1", "2", "3"]), default="1")
 @click.option("-o", "--output", help="Path to output file", type=click.Path(False))
 @click.argument("data", type=click.Path(True, True, False), required=True)
 def create(image: str, key: str, compress: str, pack: str, output: str, data: str):
@@ -24,13 +24,13 @@ def create(image: str, key: str, compress: str, pack: str, output: str, data: st
     write_steg(data, image, key, compress, pack, output)
 
 
-@steg.command("extract", help="Extract stegnograph")
+@steg.command("extract", help="Extract steganograph")
 @click.option("-k", "--key", help="The authentication key", type=str, default=Header.default_key)
 @click.option("-o", "--output", help="Path to output file (default is stdout)", type=click.Path(False))
-@click.argument("stegnograph", required=True, type=click.Path(True, True, False))
-def extract(key: str, output: str, stegnograph: str):
+@click.argument("steganograph", required=True, type=click.Path(True, True, False))
+def extract(key: str, output: str, steganograph: str):
     output = output if output else "stdout"
-    extract_steg(stegnograph, output, key)
+    extract_steg(steganograph, output, key)
 
 
 if __name__ == "__main__":
