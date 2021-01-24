4d049ab5d2a7a4bca571f57ae0085008ff60f43a
Nguyen Thai Binh <bincity2003@gmail.com>
2021-01-24 19:40:00 +0800

    Add setup script

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as rm:
+    full_description = rm.read()
+
+setup(
+    name="StegLibrary-pkg-MunchDev",
+    version="1.0.0",
+    author="Nguyen Thai Binh",
+    author_email="binhnt.mdev@gmail.com",
+    description="A package implementing and extending on steganography",
+    long_description=full_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MunchDev/StegLibrary",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
