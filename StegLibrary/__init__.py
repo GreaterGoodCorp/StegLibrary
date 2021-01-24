162ba60e2649c16a191beb5baa90c5e15b2213aa
Nguyen Thai Binh <bincity2003@gmail.com>
2021-01-25 06:25:40 +0800

    Add handle for absolute paths

--- StegLibrary/__init__.py
+++ StegLibrary/__init__.py
@@ -1,2 +1,15 @@
+import os
+
+steg_absolute_path = os.path.abspath(os.path.dirname(__file__))
+
+def create_abspath(*rel_path):
+    return os.path.join(steg_absolute_path, *rel_path)
+
+def check_abspath(abspath):
+    return os.path.isabs(abspath)
+
+def split_path(path):
+    return os.path.split(path)
+
 from .header import *
 from .steglib import *
