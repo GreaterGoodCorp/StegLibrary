d4a7fb15a4bc56d255453b5e96a6c5f84d3360bc
Nguyen Thai Binh <bincity2003@gmail.com>
2021-01-31 10:33:23 +0800

    Remove status bar functionality

--- StegLibrary/gui_action.py
+++ StegLibrary/gui_action.py
@@ -13,9 +13,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.register_logic()
 
     def register_logic(self):
-        # Let the user know that the application is ready
-        self.change_status("Ready")
-
         # Add function for Close button
         self.action_close.triggered.connect(self.close)
 
@@ -26,6 +23,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Add function for Select (Input) button
         self.button_input.clicked.connect(self.select_input)
+
+        # Add function for Select (Image) button
+        self.button_image.clicked.connect(self.select_image)
         pass
 
     def select_input(self):
@@ -52,8 +52,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.label_input_status.setText("Valid steganograph")
             # Set colour label
             self.label_input_status.setStyleSheet("QLabel { color : green; }")
-            # Set status
-            self.change_status("A valid steganograph is selected")
             # Disable field_image, button_image and check_showim
             self.field_image.setDisabled(1)
             self.button_image.setDisabled(1)
@@ -72,15 +70,37 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             # Set colour label
             self.label_input_status.setStyleSheet("QLabel { color : green; }")
             # Set status
-            self.change_status("A file is selected")
             # Disable params check
             self.check_nowrite.setDisabled(1)
             self.check_stdout.setDisabled(1)
             self.write_output("File is ready for creation!")
 
 
-    def change_status(self, status: str):
-        self.statusbar.showMessage("Status: " + status)
+    def select_image(self):
+        # Ask user to choose a file
+        file_name = QtWidgets.QFileDialog.getOpenFileName()[0]
+
+        # If the user does not select any file
+        # exit the routine
+        if file_name is None or len(file_name) == 0:
+            return
+
+        # Display text output
+        self.write_output("Image selected at: " + file_name)
+
+        # Show the path to file
+        self.field_image.setText(file_name)
+
+        # Validate image
+        try:
+            steg.validate_image_file(file_name)
+        except ImageFileValidationError as e:
+            self.label_image_status.setText("Invalid image")
+            self.label_image_status.setStyleSheet("QLabel { color: red; }")
+            self.write_output(str(e))
+        else:
+            self.write_output("")
+
 
     def write_output(self, msg: str):
         self.text_output.appendPlainText(msg)
