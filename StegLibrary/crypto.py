bb985b822eb5b32b5c9332b70918da181ff9b773
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-04 21:56:17 +0800

    Implement Fernet creator

--- StegLibrary/crypto.py
+++ StegLibrary/crypto.py
@@ -55,3 +55,68 @@ def create_kdf(salt: bytes) -> PBKDF2HMAC:
         iterations=10000,
         backend=default_backend(),
     )
+
+
+def build_fernet(key: bytes) -> Fernet:
+    """Build a Fernet object to encrypt or decrypt data from the key.
+
+    ### Positional arguments
+
+    - key (bytes)
+        - A derived key (can be derived using the KDF from make_kd())
+
+    ### Returns
+
+    A Fernet object
+
+    ### Raises
+
+    - TypeError
+        - Raised when the parametres given are in incorrect types
+    """
+    # Type checking
+    if not isinstance(key, bytes):
+        raise TypeError(f"The key must be in bytes (given {type(key)})")
+
+    # Initialise and return a Fernet object
+    return Fernet(key)
+
+def extract_raw_salt(salt_str: str) -> bytes:
+    """Extract raw salt from the given salt string.
+
+    ### Positional arguments
+
+    - salt_str (str)
+        - A salt string
+
+    ### Returns
+
+    - bytes
+        The salt from the salt string
+
+    ### Raises
+
+    - TypeError
+        - Raised when the parametres given are in incorrect types
+
+    - ValueError
+        - Raised when the parametres given are invalid
+    """
+    # Type checking
+    if not isinstance(salt_str, str):
+        raise TypeError(f"The salt must be a string (given {type(salt_str)})")
+
+    # Length checking
+    # A 16-byte salt when encoded produces a 24-character string
+    if len(salt_str) != 24:
+        raise ValueError("Invalid salt string")
+
+    # Extract salt
+    # 1. Decoding guard: shield against invalid salt string
+    try:
+        # Decode string from UTF-8 before passing on
+        # Return the base64-decoded bytes
+        return b64decode(bytes(salt_str, "utf-8"))
+    except Error:
+        # Missing padding, caused when the salt string is corrupted
+        raise ValueError("Invalid salt string")
