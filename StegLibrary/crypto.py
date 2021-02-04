dc0e1ce570a1e5ead50c884e9f8be76bfbb0617d
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-04 21:54:51 +0800

    Import required modules

--- StegLibrary/crypto.py
+++ StegLibrary/crypto.py
@@ -1,1 +1,9 @@
+from base64 import b64encode, b64decode
+from binascii import Error
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from os import urandom
+from typing import Tuple
 
