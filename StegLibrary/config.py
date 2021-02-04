19849aa11f1a433cce9a93832017c572fc8879ce
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-04 21:57:34 +0800

    Implement SteganographyConfig class

---
+++ StegLibrary/config.py
@@ -0,0 +1,9 @@
+class SteganographyConfig(object):
+    available_compression = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    available_density = [1, 2, 3]
+
+    default_compression = 9
+    default_density = 1
+    default_auth_key = "bGs21Gt@31"
+
+    flag_close_on_exit = True
