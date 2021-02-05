dbf67c5cdde9036b3d1a080ea26186c322044610
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-05 17:37:57 +0800

    Rewrite sub-package structure

--- StegLibrary/gui/__init__.py
+++ StegLibrary/gui/__init__.py
@@ -1,2 +1,12 @@
-from .gui import Ui_MainWindow
-from .gui_action import execute_gui
+# Impoprt GUI frame
+from .gui import *
+
+# Import GUI action implementation
+from .gui_action import *
+
+# Import GUI API
+from .gui_api import *
+
+__all__ = [
+    execute_gui,
+]
