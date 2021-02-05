b472de905bf26789199871247be05a53e703310e
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-05 17:34:16 +0800

    Rewrite package structure

---
+++ StegLibrary/__init__.py
@@ -0,0 +1,28 @@
+# Import class SteganographyConfig
+from .config import SteganographyConfig
+
+# Import configuration object
+cfg = SteganographyConfig
+
+# Import helper functions
+from .helper import *
+
+# Import crypto functions
+from .crypto import *
+
+# Import class Header
+from .header import Header
+
+# Import expose API
+from .steglib import *
+
+# Import GUI execute function
+from .gui import execute_gui
+
+# Define import * functionality
+# Import all only imports main API
+# All other classes, objects must be imported by name
+__all__ = [
+    write_steg,
+    extract_steg,
+]
