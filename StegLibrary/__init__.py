baa29ccec6961e362a5f011c01d56d30cfcb6c71
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-05 23:00:05 +0800

    Fix top-level package structure

--- StegLibrary/__init__.py
+++ StegLibrary/__init__.py
@@ -1,25 +1,10 @@
-# Import class SteganographyConfig
-from .config import SteganographyConfig
-
-# Import helper functions
-from .helper import *
-
-# Import crypto functions
-from .crypto import *
-
-# Import class Header
-from .header import Header
-
-# Import expose API
-from .steglib import *
-
-# Import GUI execute function
-from .gui import execute_gui
+# Import expose API functions
+from .steglib import write_steg, extract_steg
 
 # Define import * functionality
 # Import all only imports main API
 # All other classes, objects must be imported by name
 __all__ = [
-    write_steg,
-    extract_steg,
+    "write_steg",
+    "extract_steg",
 ]
