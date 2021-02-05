dd4b052cec07af582015891aa19577e1ce77ebbb
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-05 17:34:36 +0800

    Add notes

--- StegLibrary/config.py
+++ StegLibrary/config.py
@@ -1,3 +1,6 @@
+# This file defines SteganographyConfig class, which is used to
+# configure all operations of the Steganography Library.
+
 class SteganographyConfig(object):
     available_compression = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     available_density = [1, 2, 3]
