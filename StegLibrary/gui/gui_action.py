6248cdc57bca68c456fd86a813eaa7764ab49220
Nguyen Thai Binh <bincity2003@gmail.com>
2021-02-06 10:14:57 +0800

    Improve import clarity

--- StegLibrary/gui/gui_action.py
+++ StegLibrary/gui/gui_action.py
@@ -1,11 +1,11 @@
 # Builtin modules
 from os import path
+from sys import stdout
 from webbrowser import open as webopen
 
 # Internal modules
 from StegLibrary import write_steg, extract_steg
 from StegLibrary.helper import err_imp, raw_open
-from StegLibrary.core.header import Header
 from StegLibrary.core.errors import UnrecognisedHeaderError, SteganographyError
 from StegLibrary.core.steg import extract_header
 from StegLibrary.gui import Ui_MainWindow
