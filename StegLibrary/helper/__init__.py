592716a71926a115b670435f3c352f862d4bbbd3
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-02-07 09:43:16 +0800

    Expose another API function

--- StegLibrary/helper/__init__.py
+++ StegLibrary/helper/__init__.py
@@ -1,7 +1,7 @@
 # Import expose API functions
 from .bit_op import is_bit_set, set_bit, unset_bit
 from .console_op import err_imp
-from .image_op import show_image
+from .image_op import show_image, open_image
 from .file_op import raw_open
 
 # Define import * functionality
@@ -13,5 +13,6 @@ __all__ = [
     "unset_bit",
     "err_imp",
     "show_image",
+    "open_image",
     "raw_open",
 ]
