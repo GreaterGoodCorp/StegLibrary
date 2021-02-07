54e901c8be8d9048d3e6be79201c22464647fdc7
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-02-07 09:42:19 +0800

    Implement image open function

--- StegLibrary/helper/image_op.py
+++ StegLibrary/helper/image_op.py
@@ -13,6 +13,44 @@ except ImportError:
     exit(1)
 
 
+def open_image(file: Union[RawIOBase, BufferedIOBase]):
+    """Open an Image instance from the file object.
+
+    ### Positional arguments
+
+    - file (RawIOBase | BufferedIOBase)
+        - A readable fileobject
+
+    ### Returns
+
+    A PIL.Image.Image instance of the image in file
+
+    ### Raises
+
+    - TypeError
+        - Raised when the parametres are of incorrect types
+
+    - IOError
+        - Raised when file is unreadable
+
+    - UnidentifiedImageError
+        - Raised when the image cannot be retrieved from file
+    """
+    # Type checking
+    if not isinstance(file, (RawIOBase, BufferedIOBase)):
+        raise TypeError("File must be a binary fileobject")
+
+    # Check if file is readable
+    if not file.readable():
+        raise IOError("File is unreadable")
+
+    # Attempt to open and return Image
+    try:
+        return Image.open(file)
+    except UnidentifiedImageError as e:
+        raise e
+
+
 def show_image(image: Image.Image) -> None:
     """Show this Image object on screen
 
