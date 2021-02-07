89871a88aa548b507a388d4bea0220a98580430f
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-02-07 16:10:28 +0800

    Add type hinting

--- StegLibrary/core/config.py
+++ StegLibrary/core/config.py
@@ -1,14 +1,18 @@
 # This file defines SteganographyConfig class, which is used to
 # configure all operations of the Steganography Library.
 
+# Builtin modules
+from typing import List
+
+
 class SteganographyConfig(object):
-    available_compression = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    available_density = [1, 2, 3]
+    available_compression: List[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    available_density: List[int] = [1, 2, 3]
 
-    default_compression = 9
-    default_density = 1
-    default_auth_key = "bGs21Gt@31"
+    default_compression: int = 9
+    default_density: int = 1
+    default_auth_key: str = "bGs21Gt@31"
 
-    flag_close_on_exit = True
-    flag_show_image_on_completion = False
-    flag_fopen_mode = "rb"
+    flag_close_on_exit: bool = True
+    flag_show_image_on_completion: bool = False
+    flag_fopen_mode: bool = "rb"
